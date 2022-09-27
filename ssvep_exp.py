ea53a2ecdae30144916b6d1e48c5bc0e10b33c70
Aarin Salot <aarin.salot@berkeley.edu>
2022-09-27 00:14:45 -0700

    Rough version complete

--- ssvep_exp.py
+++ ssvep_exp.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame.constants
 import pygame.locals
+import time 
 
 
 # Experiment Class 
@@ -8,6 +9,8 @@ import pygame.locals
 class Experiment: 
 
 	def __init__(self, width, height): 
+		start_time = time.time()
+
 		pygame.init()
 		self.width = width
 		self.height = height
@@ -18,42 +21,47 @@ class Experiment:
 		self.left_image = pygame.image.load("left_image.png")
 		self.right_image = pygame.image.load("right_image.png")
 		self.background = pygame.image.load("background.jpg")
-		self.rounds = 0
-		self.left_rep = 10 
-		self.right_rep = 7.5
+
 		end = False 
+
+		
+		self.clock.tick(60)
+		self.screen.blit(self.background, (0,0))
+		self.screen.blit(self.left_image, (100, 200))
+		self.screen.blit(self.right_image, (700, 200))
 
 
 		header = pygame.display.set_caption("Welcome to the SSVEP experiment, please focus at a target!")
 
-
+		off1 = False 
+		off2 = False 
 		while end != True:
 			pygame.display.flip()
-			self.clock.tick(60)
-			self.screen.blit(self.background, (0,0))
+			if (int(round((time.time() - start_time) % 0.05, 0)) == 0 and not off1):
+				self.screen.blit(self.background, (0,0))
+				pygame.display.flip()
+				off1 = True 
+			elif (int(round((time.time() - start_time) % 0.1, 0)) == 0 and off1):
+				self.screen.blit(self.left_image, (100, 200))
+				pygame.display.flip()
+				off1 = False
+			pygame.display.flip()
+			if (int(round((time.time() - start_time) % 0.1, 0)) == 0 and not off2):
+				self.screen.blit(self.background, (0,0))
+				pygame.display.flip()
+				off2 = True 
+			elif (int(round((time.time() - start_time) % 0.2, 0)) == 0 and off2):
+				self.screen.blit(self.right_image, (700, 200))
+				pygame.display.flip()
+				off2 = False
+			pygame.display.flip()
 
-
-			
-
-
-
-
-			self.screen.blit(self.left_image, (100, 200))
-			self.screen.blit(self.right_image, (700, 200))
 
 			# This for loop ensures that if the user closes the pygame window, then the game will end.  
 
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					end = True
-
-
-			if self.rounds > 5: 
-				end = True 
-
-
-
-
 
 
 
