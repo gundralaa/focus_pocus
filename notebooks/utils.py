30525e5df462347ec655b465f4d4271a0b917c25
gundralaa <gundralaa@berkeley.edu>
2022-09-26 20:14:48 -0700

    changed utils

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -6,8 +6,8 @@ from brainflow.board_shim import BoardShim
 def create_mne_info(board_id):
     eeg_ch_ind = BoardShim.get_eeg_channels(board_id)
     sf = BoardShim.get_sampling_rate(board_id)
-    ch_name = ['time'] + ['marker'] + BoardShim.get_eeg_names(board_id)
-    ch_type = ['misc'] * 2 + ['eeg'] * len(eeg_ch_ind)
+    ch_name = BoardShim.get_eeg_names(board_id) + ['marker']
+    ch_type = ['eeg'] * len(eeg_ch_ind) + ['misc']
     return mne.create_info(ch_name, sf, ch_type)
 
 def data_to_mne_raw(board_id, data):
